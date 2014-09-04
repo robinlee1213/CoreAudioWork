327035de9a1a49be2f9cdaf14b00d815e2e9b03f
panthesingh <Panthe@panthes-mbp.home>
2014-09-04 02:57:35 -0700

    Initial Commit

---
+++ CoreAudioWork/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CoreAudioWork
+//
+//  Created by Panthe on 9/4/14.
+//  Copyright (c) 2014 panthesingh. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ CoreAudioWork/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  CoreAudioWork
+//
+//  Created by Panthe on 9/4/14.
+//  Copyright (c) 2014 panthesingh. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
